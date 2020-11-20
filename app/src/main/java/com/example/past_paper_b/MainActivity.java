c47075f4eb1f6c919a26dd12de65b3d0a416d1b0
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-11-20 05:53:03 +0530

    ADD: inital files created

---
+++ app/src/main/java/com/example/past_paper_b/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.past_paper_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
